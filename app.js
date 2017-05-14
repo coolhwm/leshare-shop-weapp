04e32f96d8eb91eb37aee5fb4525f22c05e07add
coolhwm <coolhwm@qq.com>
2017-05-14 12:04:28 +0800

    promise 改造

--- app.js
+++ app.js
@@ -4,37 +4,42 @@ var wxApi = require('./class/utils/wxApi')
 var wxRequest = require('./class/utils/wxRequest')
 App({
   onLaunch: function () {
-    this.checkLogin();
+    this.checkLogin()
+      .then(this.userInit)
+      .catch(() => {
+        this.userLogin()
+          .then(this.getSession)
+          .then(this.getUserInfo)
+          .then(this.checkUserInfo)
+          .then(this.decodeUserInfo);
+      });
   },
+  
   //检查登录状态
   checkLogin: function () {
-    let thirdSessionId = wx.getStorageSync("thirdSessionId");
-    let user = wx.getStorageSync("userInfo");
-    if (thirdSessionId && user) {
-      wxApi.checkSession().then(res => {
-        this.globalData.userInfo = user;
-        this.globalData.userId = user.id;
-        return false;
-      }).catch(res => {
-        this.login();
-      });
-    }
-    else {
-      this.login();
-    }
+    console.info('检查用户登录情况');
+    return new Promise((resolve, reject) => {
+      let thirdSessionId = wx.getStorageSync("thirdSessionId");
+      let user = wx.getStorageSync("userInfo");
+      if (thirdSessionId && user) {
+        wxApi.checkSession().then(res => {
+          resolve(user);
+        }, reject);
+      }
+      else {
+        reject();
+      }
+    });
   },
 
-  //用户登录
-  login: function () {
-    this.userLogin()
-      .then(this.getSession)
-      .then(this.getUserInfo)
-      .then(this.checkUserInfo)
-      .then(this.decodeUserInfo);
+  userInit: function(user){
+    console.info('用户已登录成功', user)
+    this.globalData.userInfo = user;
+    this.globalData.userId = user.id;
   },
-
 
   userLogin: function () {
+    console.info('用户尚未登录，获取用户js_code');
     return new Promise((resolve, reject) => {
       wxApi.wxLogin().then(res => {
         if (!res.code) {
@@ -49,6 +54,7 @@ App({
   },
 
   getSession: function (jsCode) {
+    console.info('获取用户thirdSessionId');
     return new Promise((resolve, reject) => {
       let url = `${this.globalData.baseUrl}/customers/session`;
       let param = { code: jsCode };
@@ -59,7 +65,7 @@ App({
           reject("thirdSessionId获取失败");
         }
         else {
-          console.info(`3rd_sessionId=${thirdSessionId}`);
+          console.info(`thirdSessionId=${thirdSessionId}`);
           //缓存3rd_sessionId
           wx.setStorageSync('thirdSessionId', thirdSessionId);
           resolve(thirdSessionId);
@@ -69,6 +75,7 @@ App({
   },
 
   getUserInfo: function (thirdSessionId) {
+    console.info('获取用户基本信息');
     return new Promise((resolve, reject) => {
       wxApi.wxGetUserInfo().then(res => {
         res["thirdSessionId"] = thirdSessionId;
@@ -78,6 +85,7 @@ App({
   },
 
   checkUserInfo: function (rawUser) {
+    console.info('检查用户信息完整性', rawUser);
     return new Promise((resolve, reject) => {
       let url = `${this.globalData.baseUrl}/customers/checkUserInfo`;
       let param = {
@@ -98,6 +106,7 @@ App({
   },
 
   decodeUserInfo: function (rawUser) {
+    console.info('解密并保存用户信息');
     return new Promise((resolve, reject) => {
       let url = `${this.globalData.baseUrl}/customers/decodeUserInfo`;
       let param = {
@@ -109,11 +118,16 @@ App({
       wxRequest.getRequest(url, param).then(res => {
         //解密成功，缓存数据
         var user = res.data.data;
-        console.info(user);
-        this.globalData.userInfo = user;
-        wx.setStorageSync("userInfo", user);
-        //临时user_id
-        this.globalData.userId = user.id;
+        if (user) {
+          console.info(user);
+          this.globalData.userInfo = user;
+          wx.setStorageSync("userInfo", user);
+          //临时user_id
+          this.globalData.userId = user.id;
+        }
+        else {
+          console.error("用户信息解密失败", res);
+        }
       }, reject);
     });
   },
