c3db4d85cf7bec4c9d3d5ae42bdcd1584c093a6c
coolhwm <coolhwm@qq.com>
2017-06-23 22:22:28 +0800

    重构店铺详情页面

--- pages/shop/detail/detail.js
+++ pages/shop/detail/detail.js
@@ -1,5 +1,5 @@
-import { Http } from "../../../class/utils/Http.js";
 import ShopService from "../../../class/service/ShopService";
+import Tips from "../../../class/utils/Tips";
 
 const app = getApp();
 const shopService = new ShopService();
@@ -14,8 +14,22 @@ Page({
       this.setData({ shop: data });
     });
   },
-  
-  onMoreTap: function (event) {
 
+  onPhoneCall: function (event) {
+    const phone = this.data.shop.phone;
+    wx.makePhoneCall({
+      phoneNumber: phone
+    });
+  },
+
+  onOpenLocation: function (event) {
+    const longitude = 119.340685;
+    const latitude = 26.105099;
+    wx.openLocation({
+      latitude: latitude,
+      longitude: longitude,
+      name: this.data.shop.name,
+      address: this.data.shop.describe
+    });
   }
 })
